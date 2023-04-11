4295e66b02f9a400dc503d7a6ea87c5af5edc99b
ruicerqueira <ruicerqueira647@gmail.com>
2023-04-11 15:59:17 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'blogPessoal',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
